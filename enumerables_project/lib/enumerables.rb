fe5815ac96b81a194bb7f884e4fef42f8668a4c9
remnantsofred <remnantsofred@gmail.com>
2022-10-17 14:02:07 -0700

    first commit

---
+++ enumerables_project/lib/enumerables.rb
@@ -0,0 +1,20 @@
+class Array
+
+    def my_each(&prc)
+        i = 0
+        while i < self.length
+            prc.call(self[i])
+            i += 1
+        end
+        self
+    end
+
+    def my_select(&prc)
+        selected = []
+        self.my_each(&prc) 
+            
+            
+    end
+
+
+ends
